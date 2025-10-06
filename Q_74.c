a2e0f114d8fe3b43906f06348101b77ab51068d5
batraronit07-ai <batra.ronit07@gmail.com>
2025-10-06 11:17:13 +0530

    Day 37

--- Q_74.c
+++ Q_74.c
@@ -13,4 +13,48 @@ Output 1:
 
 */
 
+#include <stdio.h>
 
+int main()
+{
+    int i, j, rows, columns;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &columns);
+
+    int matrix[rows][columns];
+
+    printf("Enter the values of the matrix:\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int transpose[columns][rows];
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for (i = 0; i < columns; i++)
+    {
+        for (j = 0; j < rows; j++)
+        {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
